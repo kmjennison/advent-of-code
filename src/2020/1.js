b6410e7b9393e1f3bfffe12c6163b6a1807c8666
Kevin Jennison <kevin.jennison1@gmail.com>
2020-12-04 15:38:22 -0500

    Day 1 v1

--- src/2020/1.js
+++ src/2020/1.js
@@ -1,7 +1,31 @@
+import { splitByNewLine } from '../utils'
+
+// Goal: find the the product of the two entries that
+// sum to 2020.
 export const part1 = (input) => {
-  return 'todo'
+  const entries = splitByNewLine(input).map((str) => Number.parseInt(str, 10))
+  for (let i = 0; i < entries.length; i += 1) {
+    for (let j = 0; j < entries.length; j += 1) {
+      if (i !== j && entries[i] + entries[j] === 2020) {
+        return entries[i] * entries[j]
+      }
+    }
+  }
+  throw new Error('Something is wrong.')
 }
 
+// Goal: find the the product of the three entries that
+// sum to 2020.
 export const part2 = (input) => {
-  return 'todo'
+  const entries = splitByNewLine(input).map((str) => Number.parseInt(str, 10))
+  for (let i = 0; i < entries.length; i += 1) {
+    for (let j = 0; j < entries.length; j += 1) {
+      for (let k = 0; k < entries.length; k += 1) {
+        if ((i !== j) !== k && entries[i] + entries[j] + entries[k] === 2020) {
+          return entries[i] * entries[j] * entries[k]
+        }
+      }
+    }
+  }
+  throw new Error('Something is wrong.')
 }
---
+++ src/utils.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const splitByNewLine = (input) => input.split(/\r?\n/)
