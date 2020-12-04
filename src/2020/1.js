f3c21be139d819d6360566f754ce5399bc5a5d7d
Kevin Jennison <kevin.jennison1@gmail.com>
2020-12-04 15:44:16 -0500

    Day 1 v2

--- src/2020/1.js
+++ src/2020/1.js
@@ -2,13 +2,14 @@ import { splitByNewLine } from '../utils'
 
 // Goal: find the the product of the two entries that
 // sum to 2020.
+// Answer: 121396
 export const part1 = (input) => {
   const entries = splitByNewLine(input).map((str) => Number.parseInt(str, 10))
   for (let i = 0; i < entries.length; i += 1) {
-    for (let j = 0; j < entries.length; j += 1) {
-      if (i !== j && entries[i] + entries[j] === 2020) {
-        return entries[i] * entries[j]
-      }
+    const diff = 2020 - entries[i]
+    const matchIndex = entries.indexOf(diff)
+    if (matchIndex > -1 && matchIndex !== i) {
+      return entries[i] * diff
     }
   }
   throw new Error('Something is wrong.')
@@ -16,14 +17,15 @@ export const part1 = (input) => {
 
 // Goal: find the the product of the three entries that
 // sum to 2020.
+// Answer: 73616634
 export const part2 = (input) => {
   const entries = splitByNewLine(input).map((str) => Number.parseInt(str, 10))
   for (let i = 0; i < entries.length; i += 1) {
     for (let j = 0; j < entries.length; j += 1) {
-      for (let k = 0; k < entries.length; k += 1) {
-        if ((i !== j) !== k && entries[i] + entries[j] + entries[k] === 2020) {
-          return entries[i] * entries[j] * entries[k]
-        }
+      const diff = 2020 - entries[i] - entries[j]
+      const matchIndex = entries.indexOf(diff)
+      if (matchIndex > -1 && (matchIndex !== i) !== j) {
+        return entries[i] * entries[j] * diff
       }
     }
   }
